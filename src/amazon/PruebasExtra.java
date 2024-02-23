2478ef0abcd128ca56b925d16747efeb62c1bc22
Angycht <angytc98@gmail.com>
2024-02-23 10:24:36 +0100

    quite codigo que sobra

--- src/amazon/PruebasExtra.java
+++ src/amazon/PruebasExtra.java
@@ -117,44 +117,7 @@ public class PruebasExtra {
 
 		} while (fin);
 
-		// TODO Auto-generated method stub
-		Cupon c1 = new Cupon(50.0);
-		Cupon c2 = new Cupon(50.0);
-		Cupon c3 = new Cupon(50.0);
-		Cupon c4 = new Cupon(50.0);
-		Cupon c5 = new Cupon(50.0);
-		Cupon c6 = new Cupon(50.0);
-		Cupon c7 = new Cupon(50.0);
-
-		System.out.println(c1.getIdentificador());
-
-		System.out.println(c2.getIdentificador());
-
-		System.out.println(c3.getIdentificador());
-		System.out.println(c4.getIdentificador());
-		System.out.println(c5.getIdentificador());
-		System.out.println(c6.getIdentificador());
-		System.out.println(c7.getIdentificador());
-
-		/*
-		 * c1.gastarSaldo1(20); System.out.println(c1.getSaldo());
-		 * System.out.println(c1.getIdentificador()); System.out.println(c2.getSaldo());
-		 * System.out.println(c2.getIdentificador()); Cupon nuevoCupon =
-		 * Cupon.fusionarCupones(c1, c2); System.out.println(nuevoCupon.getSaldo());
-		 * System.out.println(nuevoCupon.getIdentificador());
-		 * System.out.println(c1.getSaldo()); System.out.println(c2.getSaldo());
-		 * 
-		 * /* System.out.println(c1.getSaldo());
-		 * 
-		 * System.out.println(c2.getSaldo()); Cupon nuevo = Cupon.fusionarCupones(c1,
-		 * c2); System.out.println(c1.getSaldo());
-		 * 
-		 * System.out.println(c2.getSaldo()); System.out.println(nuevo.getSaldo());
-		 */
-
-		c1.aniadirSaldo(10);
-		c1.gastarSaldo(10);
-		Cupon.mostrarInformacion(c1);
+	
 	}
 
 }
